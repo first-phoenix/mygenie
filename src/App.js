3efb4da68504fe7efb795a7b3e1ecd5a7b7fc57b
first-phoenix <iswarajkhan@gmail.com>
2024-03-20 21:35:37 +0530

    combined dashboard and chat component in mainpage

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import Chatbot from 'react-chatbot-kit';
 import 'react-chatbot-kit/build/main.css';
-import config from './Chatbot/config';
-import ActionProvider from './Chatbot/ActionProvider';
-import MessageParser from './Chatbot/MessageParser';
 import './App.css';
-import Dashboard from './Dashboard/Dashboard';
+import MainPage from './MainPage/MainPage';
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <div className="dashboard">
-          <Dashboard />
-        </div>
-        <div className="chatbot">
-          <Chatbot
-            config={config}
-            messageParser={MessageParser}
-            actionProvider={ActionProvider}
-          />
-        </div>
-      </div>
+    <div className="MainPage">
+      <MainPage/>
     </div>
   );
 }
---
+++ src/MainPage/MainPage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Chatbot from 'react-chatbot-kit';
+import 'react-chatbot-kit/build/main.css';
+import config from '../Chatbot/config';
+import ActionProvider from '../Chatbot/ActionProvider';
+import MessageParser from '../Chatbot/MessageParser';
+import '../App.css';
+import Dashboard from '../Dashboard/Dashboard';
+
+function MainPage() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="dashboard">
+          <Dashboard />
+        </div>
+        <div className="chatbot">
+          <Chatbot
+            config={config}
+            messageParser={MessageParser}
+            actionProvider={ActionProvider}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
