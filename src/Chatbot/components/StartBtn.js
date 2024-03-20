7baf2b6aa58db5f2083e91fc02d52d40caffd735
Kushankur Das <daskushankur990@gmail.com>
2024-03-20 17:37:13 +0530

    Reapply "css fixed"
    This reverts commit 6d0e2b54435af6f22a2420e724ac79e9c052ab77.

--- src/Chatbot/components/StartBtn.js
+++ src/Chatbot/components/StartBtn.js
@@ -7,7 +7,7 @@ export default function StartBtn(props) {
     }
 
     return (
-        <div>
+        <div className='threeWidgets'>
             <button className='measures-btn' onClick={() => initialMessage()}>Suggested Prompt -  What measures are in place to ensure consistency in material quality from different suppliers?</button>
             <button className='measures-btn trends-btn' onClick={() => initialMessage()}>Suggested Prompt - Latest trends in the packaging within the CPG industry?</button>
             <button className='measures-btn quality-btn' onClick={() => initialMessage()}>Suggested Prompt -  What are key quality control checkpoints in the packaging production process</button>
