6878cbfc5ce3e6fb3898eb3ecbc8c48a6a400dd8
first-phoenix <iswarajkhan@gmail.com>
2024-03-22 02:55:24 +0530

    added responsiveness and tiles for home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -11,14 +11,22 @@ const Home = () => {
             <img src={logoImage} alt='logo' className='logo'></img>
             <div className="home-container">
                 <h2 className='welcomeNote'>Hi! Jane!</h2>
-                <div className="tile">
+                <div className="tile tile1">
                     <h2>Start your next research or project here</h2>
                     <Link to='/chathere'><img src={clickImage} alt='click here' className='clickImage'></img></Link>
                 </div>
-                <p>-----------------------------------------------------------------------------------</p>
+                <p className='lineSperation'>-----------------------------------------------------------------------------------</p>
                 <h2 className='welcomeNote'>Start your day</h2>
-                <div className="tile">
+                <div className="tile tile2">
                     <h2>Most important organization news</h2>
+                    <img src={clickImage} alt='click here' className='clickImage'></img>
+                </div>
+                <div className="tile tile3">
+                    <h2>Go through recent technology trends</h2>
+                    <img src={clickImage} alt='click here' className='clickImage'></img>
+                </div>
+                <div className="tile tile4">
+                    <h2>Tech Innovations around the globe</h2>
                     <img src={clickImage} alt='click here' className='clickImage'></img>
                 </div>
             </div>
