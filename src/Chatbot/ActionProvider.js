14e98327ae5f25f6d89f1e97d03c568e01ea6969
Kushankur Das <daskushankur990@gmail.com>
2024-04-01 15:51:49 +0530

    Promts Changed

--- src/Chatbot/ActionProvider.js
+++ src/Chatbot/ActionProvider.js
@@ -1,92 +1,95 @@
-import React from 'react';
+import React from "react";
 
 const ActionProvider = ({ createChatBotMessage, setState, children }) => {
-
-    const initialMessage = () => {
-        const message = createChatBotMessage(
-            <>
-                Hi Jane – certainly, I went through 4 relevant research decks published within the last quarter and here is a summarized version of the latest trends: <br></br><br></br>Philippines Baby Food Market Assessment, Innovative Food Packaging,
-            </>,{
-            widget: "image"
-            });
-         updateState(message, "keyword1")
-
-    }
-
-    const afterFirstMessage = () => {
-        const message = createChatBotMessage(
-            <>Got it. Sounds like an exciting project – how can I help?</>
-        );
-        updateState(message, "keyword2")
-    }
-
-
-    const afterSecondMessage = () => {
-        const message = createChatBotMessage(
-            <>
-            Here are some hits from Tetrapak repositiry – tell me which one you are interested to know more about:<br></br>
-            1) In the Idaho plant a Modified Atmosphere Packaging technology was implemented for cut fruits – , slowed down the ripening process | Client: Danone<br></br>
-            POC: Boris Hoot<br></br>
-            Link to Success Story & Compliance/Regulatory Checklist<br></br>
-            
-            2) In NZ plant, we recently used Antimicrobial Packaging for seafood and extended shelf life 3x | Client: Nestle<br></br>
-            POC: Ram Hari <br></br>
-            Link to the copyrighted technology doc.<br></br>
-            </>
-        )
-        updateState(message,"keyword3")
-    }
-    const afterThirdMessage = () => {
-        const message = createChatBotMessage(
-        <>
-        Of course – Here are some prototypes available for your reference
-        </>)
-        updateState(message,"keyword4")
-    }
-
-    const finalMessage = () => {
-        const message = createChatBotMessage(
-            <>
-            We are authorized to work with the following partners, please ensure you reach out to them through the Comms team and follow the adviced protocol. <br></br>
-
-            Mail –Comms.nl@Tetrapak.com<br></br>
-
-            1. University of California, Davis - Department of Food Science and Technology<br></br>
-            2. University of Florida - Packaging Science Program<br></br>
-            3. Michigan State University - School of Packaging<br></br>
-            4. ETH Zurich - Institute of Food, Nutrition and Health<br></br>
-            5. Purdue University - Department of Food Science<br></br>
-            6. Fraunhofer Institute for Process Engineering and Packaging IVV (Germany)<br></br>
-            7. Swiss Federal Institute of Technology - Laboratory of Sustainable & Catalytic Processing<br></br>
-
-            </>
-        )
-        updateState(message)
-    }
-
-    const updateState = (message, checker) => {
-        setState((prev) => ({
-            ...prev,
-            messages: [...prev.messages, message],
-            checker,
-        }))
-    }
-
-    return (
-        <div>
-            {React.Children.map(children, (child) => {
-                return React.cloneElement(child, {
-                    actions: {
-                        initialMessage,
-                        afterFirstMessage,
-                        afterSecondMessage,
-                        afterThirdMessage,
-                        finalMessage
-                    },
-                });
-            })}
-        </div>
+  const initialMessage = () => {
+    const message = createChatBotMessage(
+      <>
+        Hi Jane – Based on the available reports was able to find the above two
+        slides with the latest trends.
+      </>,
+      {
+        widget: "image",
+      }
     );
+    updateState(message, "keyword1");
+  };
+
+  const afterFirstMessage = () => {
+    const message = createChatBotMessage(
+      <>Got it. Sounds like an exciting project – how can I help?​</>
+    );
+    updateState(message, "keyword2");
+  };
+
+  const afterSecondMessage = () => {
+    const message = createChatBotMessage(
+      <>
+        Here are some researches and reports based returns for the ​<br></br>
+        1) In the Idaho plant a Modified Atmosphere Packaging technology was
+        implemented for cut fruits – , slowed down the ripening process |
+        Client: Danone<br></br>
+        POC: Boris Hoot<br></br>
+        Link to Success Story & Compliance/Regulatory Checklist<br></br>
+        2) In NZ plant, we recently used Antimicrobial Packaging for seafood and
+        extended shelf life 3x | Client: Nestle<br></br>
+        POC: Ram Hari <br></br>
+        Link to the copyrighted technology doc.<br></br>
+      </>
+    );
+    updateState(message, "keyword3");
+  };
+  const afterThirdMessage = () => {
+    const message = createChatBotMessage(
+      <>Of course – Here are some prototypes available for your reference</>
+    );
+    updateState(message, "keyword4");
+  };
+
+  const finalMessage = () => {
+    const message = createChatBotMessage(
+      <>
+        We are authorized to work with the following partners, please ensure you
+        reach out to them through the Comms team and follow the adviced
+        protocol. <br></br>
+        Mail –Comms.nl@Tetrapak.com<br></br>
+        1. University of California, Davis - Department of Food Science and
+        Technology<br></br>
+        2. University of Florida - Packaging Science Program<br></br>
+        3. Michigan State University - School of Packaging<br></br>
+        4. ETH Zurich - Institute of Food, Nutrition and Health<br></br>
+        5. Purdue University - Department of Food Science<br></br>
+        6. Fraunhofer Institute for Process Engineering and Packaging IVV
+        (Germany)<br></br>
+        7. Swiss Federal Institute of Technology - Laboratory of Sustainable &
+        Catalytic Processing<br></br>
+      </>
+    );
+    updateState(message);
+  };
+
+  const updateState = (message, checker) => {
+    setState((prev) => ({
+      ...prev,
+      messages: [...prev.messages, message],
+      checker,
+    }));
+  };
+
+  return (
+    <div>
+      {React.Children.map(children, (child) => {
+        return React.cloneElement(child, {
+          actions: {
+            initialMessage,
+            afterFirstMessage,
+            afterSecondMessage,
+            afterThirdMessage,
+            finalMessage,
+          },
+        });
+      })}
+    </div>
+  );
 };
 
 export default ActionProvider;
--- src/Chatbot/config.js
+++ src/Chatbot/config.js
@@ -1,46 +1,56 @@
-import { createChatBotMessage } from 'react-chatbot-kit';
-import Avatar from './components/Avatar';
-import StartBtn from './components/StartBtn';
-import data from './data';
-import DipslayImage from './components/DipslayImage';
-import UserAvatar from './components/UserAvatar';
+import { createChatBotMessage } from "react-chatbot-kit";
+import Avatar from "./components/Avatar";
+import StartBtn from "./components/StartBtn";
+import data from "./data";
+import DipslayImage from "./components/DipslayImage";
+import UserAvatar from "./components/UserAvatar";
 
-const botName = 'CGS Gennie';
-const userName = 'Jane';
+const botName = "CGS Gennie";
+const userName = "Jane";
 
 const config = {
-    botName: botName,
-    initialMessages: [createChatBotMessage(`Hi ${userName} – My name is Gennie, here to help you find people, documents, data, departments in a structured and summarized format – how can I help you?​`, {
-        widget: "startBtn"
-    })],
-    customComponents: {
-        botAvatar: (props) => <Avatar {...props} />,
-        userAvatar: (props) => <UserAvatar {...props} />,
+  botName: botName,
+  initialMessages: [
+    createChatBotMessage(
+      <>
+        Hi {userName} – I’m Gennie, how can I assist you today?
+        <br />
+        <br />
+        Click here to know more about me and how I can help you?​​
+      </>,
+      {
+        widget: "startBtn",
+      }
+    ),
+  ],
+  customComponents: {
+    botAvatar: (props) => <Avatar {...props} />,
+    userAvatar: (props) => <UserAvatar {...props} />,
+  },
+  state: {
+    checker: null,
+    data,
+    userData: {
+      name: "",
+      age: "",
+      category: "",
+      product: {
+        name: "",
+        link: "",
+        imageUrl: "",
+      },
     },
-    state: {
-        checker: null,
-        data,
-        userData: {
-            name: "",
-            age: "",
-            category: "",
-            product: {
-                name: "",
-                link: "",
-                imageUrl: ""
-            }
-        }
+  },
+  widgets: [
+    {
+      widgetName: "startBtn",
+      widgetFunc: (props) => <StartBtn {...props} />,
     },
-    widgets: [
-        {
-            widgetName: "startBtn",
-            widgetFunc: (props) => <StartBtn {...props} />,
-        },
-        {
-            widgetName: "image",
-            widgetFunc: (props) => <DipslayImage {...props} />,
-        },
-    ]
+    {
+      widgetName: "image",
+      widgetFunc: (props) => <DipslayImage {...props} />,
+    },
+  ],
 };
 
 export default config;
