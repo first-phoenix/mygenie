46108ad4daa2035e517967010ba0d481854401f7
Kushankur Das <daskushankur990@gmail.com>
2024-04-01 16:36:19 +0530

    new prompts added

--- src/Chatbot/ActionProvider.js
+++ src/Chatbot/ActionProvider.js
@@ -45,7 +45,7 @@ const ActionProvider = ({ createChatBotMessage, setState, children }) => {
     updateState(message, "keyword4");
   };
 
-  const finalMessage = () => {
+  const afterFourthMessage = () => {
     const message = createChatBotMessage(
       <>
         We are authorized to work with the following partners, please ensure you
@@ -62,6 +62,122 @@ const ActionProvider = ({ createChatBotMessage, setState, children }) => {
         (Germany)<br></br>
         7. Swiss Federal Institute of Technology - Laboratory of Sustainable &
         Catalytic Processing<br></br>
+      </>
+    );
+    updateState(message, "keyword5");
+  };
+  const afterFifthMessage = () => {
+    const message = createChatBotMessage(
+      <>
+        Of course – It's important to note that regulations can change over
+        time, so it's essential to consult the relevant regulatory authorities
+        or legal experts for the most up-to-date information on compliance
+        requirements in both the Philippines and Yemen.
+        <br></br>
+        <br></br>
+        Check these sources that contain the latest regulations for Philippines:
+        <br></br>
+        <br></br>
+        <a
+          href="https://www.fda.gov.ph/draft-for-comments-adoption-of-the-philippine-national-standards-pns-and-its-recommended-code-of-practices-rcp-for-processed-food-products-as-technical-regulation/"
+          target="blank"
+        >
+          Philippine National Standards (PNS)
+        </a>
+        <br></br>
+        <a href="https://www.fda.gov.ph/" target="blank">
+          Food and Drug Administration (FDA) Philippines
+        </a>
+        <br></br>
+        <a
+          href="https://fdc.da.gov.ph/the-food-safety-act-of-2013/"
+          target="blank"
+        >
+          Philippine Food Safety Act of 2013
+        </a>
+        <br></br>
+        <a
+          href="https://www.fao.org/fao-who-codexalimentarius/en/"
+          target="blank"
+        >
+          Codex Alimentarius
+        </a>
+        <br></br>
+      </>
+    );
+    updateState(message, "keyword6");
+  };
+  const afterSixthMessage = () => {
+    const message = createChatBotMessage(
+      <>
+        Regulatory compliance requirements for baby food pouches can vary
+        significantly between countries due to differences in legislation,
+        standards, and cultural norms. Here's a general overview of what you
+        might expect in the Philippines and Yemen:
+        <br></br>
+        <strong>Phillipines:</strong>
+        <br></br>
+        1.Food Safety Standards: In the Philippines, baby food pouches must
+        comply with the Food Safety Act of 2013, which establishes safety
+        standards for all food products.
+        <br></br>
+        2.Labeling Requirements: The labeling of baby food pouches in the
+        Philippines must include important information such as the product name,
+        ingredients list, nutritional information, expiration date, and
+        instructions for use. It's essential to comply with the labeling
+        requirements set by the Food and Drug Administration (FDA) in the
+        Philippines.
+        <br></br>
+        3.Language: Labels should be in Filipino or English, or in both
+        languages, as per the regulations set by the Philippine FDA.
+        <br></br>
+        4.Design Standards: While there may not be specific regulations
+        regarding the design of baby food pouches, it's crucial to ensure that
+        the packaging is child-resistant to prevent accidental ingestion.
+        <br></br>
+        <br></br>
+        <strong>Yemen:</strong>
+        <br></br>
+        1.Food Safety Standards: Yemen may have its own food safety regulations,
+        which baby food pouches must comply with to ensure they are safe for
+        consumption.
+        <br></br>
+        2.Labeling Requirements: Like in the Philippines, labeling requirements
+        in Yemen typically include the product name, ingredients list,
+        nutritional information, expiration date, and instructions for use.
+        However, specific requirements may vary, so it's important to adhere to
+        the regulations set by the relevant authorities in Yemen.
+        <br></br>
+        3.Language: Labels should be in Arabic, as it's the official language of
+        Yemen.
+        <br></br>
+        4.Design Standards: There might be specific design standards or
+        guidelines in Yemen, particularly concerning the packaging's safety and
+        suitability for baby food products.
+        <br></br>
+        It's important to note that regulations can change over time, so it's
+        essential to consult the relevant regulatory authorities or legal
+        experts for the most up-to-date information on compliance requirements
+        in both the Philippines and Yemen. Additionally, working with local
+        partners or distributors who are familiar with the regulatory landscape
+        can be beneficial when navigating compliance issues in foreign markets.
+      </>
+    );
+    updateState(message, "keyword7");
+  };
+  const afterSeventhMessage = () => {
+    const message = createChatBotMessage(
+      <>Happy to help – here is your word document:</>
+    );
+    updateState(message,"keyword8");
+  };
+  const afterEighthMessage = () => {
+    const message = createChatBotMessage(
+      <>
+        Hi Jane, are you still there? What else can I assist you with?
+        <br></br>
+        <br></br>I can translate documents for you to local languages….and more.
+        See here for more info
       </>
     );
     updateState(message);
@@ -84,7 +200,12 @@ const ActionProvider = ({ createChatBotMessage, setState, children }) => {
             afterFirstMessage,
             afterSecondMessage,
             afterThirdMessage,
-            finalMessage,
+            afterFourthMessage,
+            afterFifthMessage,
+            afterSixthMessage,
+            afterSeventhMessage,
+            afterEighthMessage,
+            //finalMessage,
           },
         });
       })}
--- src/Chatbot/MessageParser.js
+++ src/Chatbot/MessageParser.js
@@ -1,36 +1,48 @@
-import React from 'react';
+import React from "react";
 
 const MessageParser = ({ children, actions }) => {
-    // console.log(children.props.state)
-    const { checker } = children.props.state;
-    const parse = (message) => {
-        if (checker === "keyword1") {
-            actions.afterFirstMessage();
-            children.props.state.userData.name = message;
-        }
-
-        if (checker === "keyword2") {
-            actions.afterSecondMessage();
-            children.props.state.userData.age = message;
-            children.props.state.userData.category = "kid";
-        }
-        if(checker==="keyword3"){
-            actions.afterThirdMessage();
-        }
-        if(checker==="keyword4"){
-            actions.finalMessage();
-        }
+  // console.log(children.props.state)
+  const { checker } = children.props.state;
+  const parse = (message) => {
+    if (checker === "keyword1") {
+      actions.afterFirstMessage();
+      children.props.state.userData.name = message;
     }
-    return (
-        <div>
-            {React.Children.map(children, (child) => {
-                return React.cloneElement(child, {
-                    parse: parse,
-                    actions,
-                });
-            })}
-        </div>
-    );
+
+    if (checker === "keyword2") {
+      actions.afterSecondMessage();
+      children.props.state.userData.age = message;
+      children.props.state.userData.category = "kid";
+    }
+    if (checker === "keyword3") {
+      actions.afterThirdMessage();
+    }
+    if (checker === "keyword4") {
+      actions.afterFourthMessage();
+    }
+    if (checker === "keyword5") {
+      actions.afterFifthMessage();
+    }
+    if (checker === "keyword6") {
+      actions.afterSixthMessage();
+    }
+    if (checker === "keyword7") {
+      actions.afterSeventhMessage();
+    }
+    if (checker === "keyword8") {
+      actions.afterEighthMessage();
+    }
+  };
+  return (
+    <div>
+      {React.Children.map(children, (child) => {
+        return React.cloneElement(child, {
+          parse: parse,
+          actions,
+        });
+      })}
+    </div>
+  );
 };
 
 export default MessageParser;
